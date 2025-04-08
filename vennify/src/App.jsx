e2d03eb46d5a373113cbd11b79a79efb8a34a2e7
siddhant4357 <siddhantsankesara@gmail.com>
2025-04-08 08:26:12 +0530

    changes

--- vennify/src/App.jsx
+++ vennify/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import AboutPage from './pages/AboutPage'
 import TheoryPage from './pages/TheoryPage'
@@ -10,13 +10,14 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        {/* Redirect from root to homepage */}
-        <Route path="/" element={<Navigate to="/home" replace />} />
+        <Route path="/" element={<HomePage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/theory" element={<TheoryPage />} />
         <Route path="/practice/:operation" element={<PracticePage />} />
         <Route path="/visualizer" element={<VisualizerPage />} />
+        {/* Catch all route */}
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </Router>
   )
